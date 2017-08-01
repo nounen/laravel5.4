03198cbdc91437d3252f550ee7f92e5e73e0f064
linxiaolong <1394139539@qq.com>
2017-08-01 15:50:41 +0800

    任务清单: 路由绑定模型.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -33,4 +33,9 @@ class TaskController extends Controller
 
         return redirect('/tasks');
     }
+
+    public function destroy(Request $request, $taskId)
+    {
+        //
+    }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -34,7 +34,22 @@
                                     </td>
 
                                     <td>
-                                       <!-- 待办：删除按钮 -->
+                                        <tr>
+                                            <!-- 任务名称 -->
+                                            <td class="table-text">
+                                                <div>{{ $task->name }}</div>
+                                            </td>
+
+                                            <!-- 删除按钮 -->
+                                            <td>
+                                                <form action="/task/{{ $task->id }}" method="POST">
+                                                    {{ csrf_field() }}
+                                                    {{ method_field('DELETE') }}
+
+                                                    <button>删除任务</button>
+                                                </form>
+                                            </td>
+                                        </tr>
                                     </td>
                                 </tr>
                             @endforeach
